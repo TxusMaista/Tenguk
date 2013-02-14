e84bff0c1cd07a513b9fac300f9ede7d4cb4e77f
TxusMaista <TxusMaista@TxPro.local>
2013-02-14 12:35:59 +0100

    new

---
+++ data/templates.js
@@ -0,0 +1,20 @@
+Template = (function() {
+
+  var categoria = '{{#item}}<article class="{{clase}}" onclick="{{onclick}}">\
+            {{{img}}}<h1>{{title}}</h1>\
+            <div class="bottom">{{{text}}}</div>\
+            </article>{{/item}}';
+
+  var noticia = '{{#item}}<article class="{{clase}}" onclick="{{onclick}}">\
+            {{{img}}}<div class="{{catclass}}"><h1>{{cat}}</h1></div>\
+            <div class="title">{{title}}</div>\
+            <div class="desc">{{{desc}}}</div>\
+            <div class="texto">{{{text}}}</div>\
+            </article>{{/item}}';
+
+  return{
+    	noticia: function(){ return (noticia);},
+    	categoria: function(){ return (categoria);},
+  }
+
+}).call(this);
